c78d67aa3715294edc2cfa2cbc04dda29faf315b
tsvetomirgeorgiev98 <1705848@rgu.ac.uk>
2021-01-28 16:21:17 +0000

    adding navigation

---
+++ navigation.php
@@ -0,0 +1,13 @@
+<?php
+$mydir = '.';
+$pages = scandir($mydir);
+echo '<div class="menu">';
+foreach($pages as $page){
+  if(strpos($page, '.php')){
+      $link = $mydir.'/'.$page;
+      $safeName = str_replace('.php', '', $page);
+      echo '<a href="'.$link.'">'.$safeName.'</a> &nbsp; &nbsp;';
+    }
+}
+echo '</div>';
+?>
