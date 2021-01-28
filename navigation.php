bc6d3b033432decf42dba2bd06afc853d4159e22
tsvetomirgeorgiev98 <1705848@rgu.ac.uk>
2021-01-28 16:35:00 +0000

    hate ball

--- navigation.php
+++ navigation.php
@@ -8,6 +8,12 @@ foreach($pages as $page){
       $safeName = str_replace('.php', '', $page);
       echo '<a href="'.$link.'">'.$safeName.'</a> &nbsp; &nbsp;';
     }
+	
+	if(strpos($page, '.html')){
+      $link = $mydir.'/'.$page;
+      $safeName = str_replace('.html', '', $page);
+      echo '<a href="'.$link.'">'.$safeName.'</a> &nbsp; &nbsp;';
+    }
 }
 echo '</div>';
 ?>
